f95f9a840dc216839862333e16da78af2b09d90e
Joshua Zhao <joshua.zhao0315@gmail.com>
2021-10-07 12:13:06 -0700

    figured out how to seed

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -41,7 +41,7 @@ class Api::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password, :profile_photo)
+    params.require(:user).permit(:username, :email, :password)
   end
 
   def update_user_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+demo_user = User.create(
+  username: 'demo-user',
+  email: 'demo-user@demo.com',
+  password: '123456'
+)
+
+u1 = User.create(
+  username: 'illenium',
+  email: 'illenium@gmail.com',
+  password: '123456'
+)
+
+u1_pfp = open('https://music-cloud-pro.s3.us-west-1.amazonaws.com/seeds/profile_pics/illenium.png')
+u1.profile_photo.attach(io: u1_pfp, filename: 'illenium.png')
+u1.save!
