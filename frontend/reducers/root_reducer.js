1bfa32e2c6f2b65cad0b1152a86322792847e989
Joshua Zhao <joshua.zhao0315@gmail.com>
2021-11-04 17:32:20 -0700

    clear application state upon logout

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,13 +4,21 @@ import sessionReducer from "./session_reducer";
 import errorsReducer from "./errors_reducer";
 import uiReducer from "./ui_reducer";
 import playerReducer from "./player_reducer";
+import { LOGOUT_CURRENT_USER } from "../actions/session_actions";
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
   ui: uiReducer,
   errors: errorsReducer,
   player: playerReducer
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === LOGOUT_CURRENT_USER) {
+    return appReducer(undefined, action);
+  }
+  return appReducer(state,action);
+}
 
 export default rootReducer;
